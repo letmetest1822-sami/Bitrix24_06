1156a6d93f464dfe4375e10798c258f2bb59e006
saerdem <saerdem@gmail.com>
2020-05-28 18:28:34 +0300

    first update

---
+++ src/test/java/com/cybertek/tests/ali.java
@@ -0,0 +1,8 @@
+package com.cybertek.tests;
+
+public class ali {
+    public static void main(String[] args) {
+        System.out.println("to add branch \"ali\" and push to check");
+        System.out.println("to update 1. add this line");
+    }
+}
