a18554f3e38dbd5f08778908e6f08bd32388c2ac
erkal38 <nalruvceysem1984@gmail.com>
2020-06-03 01:20:43 +0300

    erkalupdate

--- src/test/java/com/cybertek/tests/erkal.java
+++ src/test/java/com/cybertek/tests/erkal.java
@@ -1,8 +1,31 @@
 package com.cybertek.tests;
 
-public class erkal {
-    public static void main(String[] args) {
-        System.out.println("Hi Bitrix");
-        //sfdfsf
+import com.cybertek.pages.DashboardPage;
+import com.cybertek.pages.EventPage;
+import com.cybertek.pages.LoginPage;
+import com.cybertek.utilities.ConfigurationReader;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class erkal extends TestBase {
+    /*AC:User should be able to create events by clicking on Event tab under Activity Stream.
+       1-Hover over top module on the page and navigate Event module.
+       2-Click the Event module
+       3-Verify to see Event page
+     */
+    @Test
+     public void eventTest(){
+        extentLogger=report.createTest("Event Page");
+        LoginPage loginPage=new LoginPage();
+        extentLogger.info("Username:"+ ConfigurationReader.get("hr1_username"));
+        extentLogger.info("Password:"+"******");
+        loginPage.loginAsHR1();
+        new DashboardPage().navigateToModule("Activity Stream","Event");
+        EventPage eventP=new EventPage();
+        eventP.eventPage.click();
+        String expectedPage="EVENT_NAME";
+        String actualPage=eventP.eventPage.getText();
+        Assert.assertEquals(actualPage,expectedPage,"verify is Ok");
+        extentLogger.pass("Pass:");
     }
 }
