8d4511e42216d1db7ce833a838420c355873d0ca
erkal38 <nalruvceysem1984@gmail.com>
2020-06-03 15:13:30 +0300

    erkalupdate

---
+++ src/test/java/com/cybertek/pages/EventPage.java
@@ -0,0 +1,10 @@
+package com.cybertek.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class EventPage extends BasePage{
+    @FindBy(xpath = "//span[@class='feed-add-post-form-link feed-add-post-form-link-active']")
+    public WebElement eventPage;
+
+}
